e27d7fe827007667de646660b8ee200d23c51b81
Maria <mrandriamihaja@getmeelo.com>
2023-04-13 20:23:17 +0200

    Écriture des pages JSF et des backing beans associé : Page qui liste tous les comptes

---
+++ src/main/java/mg/itu/tpbanquerandriamihaja/jsf/ListeComptes.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mg.itu.tpbanquerandriamihaja.jsf;
+
+import jakarta.ejb.EJB;
+import jakarta.faces.view.ViewScoped;
+import jakarta.inject.Named;
+import java.io.Serializable;
+import java.util.List;
+import mg.itu.tpbanquerandriamihaja.ejb.GestionnaireCompte;
+import mg.itu.tpbanquerandriamihaja.entities.CompteBancaire;
+
+/**
+ *
+ * @author maria
+ */
+@Named(value = "listeComptes")
+@ViewScoped
+public class ListeComptes implements Serializable {
+
+    @EJB
+    private GestionnaireCompte gestionnaireCompte;
+
+    private List<CompteBancaire> allComptes;
+
+    
+    
+    /**
+     * Get all compte list 
+     *
+     * @return allComptes
+     */
+    public List<CompteBancaire> getAllComptes() {
+        if (allComptes == null) {
+            allComptes = gestionnaireCompte.getAllComptes();
+        }
+        return allComptes;
+    }
+}
