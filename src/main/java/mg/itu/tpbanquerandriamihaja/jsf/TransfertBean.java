1a6a1496809debb17c34c02332375ee92e853e12
Maria <mrandriamihaja@getmeelo.com>
2023-04-13 21:41:00 +0200

    Traiter les erreurs et les messages de succès

--- src/main/java/mg/itu/tpbanquerandriamihaja/jsf/TransfertBean.java
+++ src/main/java/mg/itu/tpbanquerandriamihaja/jsf/TransfertBean.java
@@ -9,6 +9,7 @@ import jakarta.enterprise.context.RequestScoped;
 import jakarta.inject.Named;
 import mg.itu.tpbanquerandriamihaja.ejb.GestionnaireCompte;
 import mg.itu.tpbanquerandriamihaja.entities.CompteBancaire;
+import mg.itu.tpbanquerandriamihaja.jsf.util.Util;
 
 /**
  *
@@ -52,9 +53,28 @@ public class TransfertBean {
     }
 
     public String transferer() {
+        boolean erreur = false;
         CompteBancaire source = gestionnaireCompte.findById(idSource);
+        if (source == null) {
+            Util.messageErreur("Aucun compte avec cet id !", "Aucun compte avec cet id !", "form:source");
+            erreur = true;
+        } else {
+            if (source.getSolde() < montant) {
+                Util.messageErreur("Solde insuffisant ! ", "Solde insuffisant !", "form:montant");
+                erreur = true;
+            }
+        }
         CompteBancaire destination = gestionnaireCompte.findById(idDestination);
+        if (destination == null) {
+            Util.messageErreur("Aucun compte avec cet id !", "Aucun compte avec cet id !", "form:destination");
+            erreur = true;
+        }
+        if (erreur) { // en cas d'erreur, rester sur la même page
+            return null;
+        }
         gestionnaireCompte.transferer(source, destination, montant);
+        // Message de succès ; addFlash à cause de la redirection.
+        Util.addFlashInfoMessage("Transfert correctement effectué");
         return "listeComptes?faces-redirect=true";
     }
 
